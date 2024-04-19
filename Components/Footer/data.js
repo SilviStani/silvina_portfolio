628811f85fbacccc39b00aba6b0a4378bc3802cd
Silvina Staniszewski <silvina.stani@gmail.com>
2024-04-19 17:45:49 -0300

    fix

--- Components/Footer/data.js
+++ Components/Footer/data.js
@@ -1,21 +1,21 @@
 export const data = [
     {
         id: 1,
-        title: "Whatsapp",
+        title: "WhatsApp",
         url: "https://wa.me/5491163632288?text=Hola!%20Me%20interesa%20información%20sobre%20las%20páginas%20Web.%0AGracias!",
         target: "_blank",
         src: ""
     },
     {
         id: 2,
-        title: "Github",
+        title: "GitHub",
         url: "https://github.com/SilviStani/",
         target: "_blank",
         src: ""
     },
     {
         id: 3,
-        title: "Linkedin",
+        title: "LinkedIn",
         url: "https://www.linkedin.com/in/silvina-staniszewski-18493276/",
         target: "_blank",
         src: ""
