0e65a4c1cfffa3ad23dd76a0439095af66eb2c5a
Silvina Staniszewski <silvina.stani@gmail.com>
2024-08-24 13:54:03 -0300

    changes on css responsive

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -11,23 +11,13 @@ const Trabajos = () => {
         <div className={styles.Left}>
           <div className={styles.stamp}>
             <h2 className={styles.title}>Proyectos</h2>
-            <div className={styles.DescContainer}>
-              <p className={styles.p}>
-                Éstas páginas fueron diseñadas y creadas con
-              </p>
-              <p className={styles.p}>React-Redux - Next.Js</p>
-              <p className={styles.p}>Css - Sass</p>
-              <p className={styles.p}>Node.Js - Express</p>
-            </div>
           </div>
-        </div>
-        <div className={styles.Right}>
           <Image
           className={styles.imgTitle}
             src={"/assets/studyings.png"}
             alt="C# knowledge"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
           />
         </div>
       </div>
