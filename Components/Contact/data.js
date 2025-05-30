405b97c39b6dab3ecd3bce2913e42eead9f0380a
SilviStani <silvina.stani@gmail.com>
2025-05-30 09:05:40 -0300

    refining scss for responsiveness: contact section

--- Components/Contact/data.js
+++ Components/Contact/data.js
@@ -1,10 +1,10 @@
 export const data = [
-  { id: 1, title: "Mail", text: "silvina.stani@gmail.com" },
-  { id: 2, title: "Provincia", text: "Córdoba" },
-  { id: 3, title: "Teléfono", text: "+54-91163632288" },
+  { id: 1, title: "Provincia", text: "Córdoba" },
+  { id: 2, title: "Teléfono", text: "+54-91163632288" },
+  { id: 3, title: "Mail", text: "silvina.stani@gmail.com" },
 ];
 export const dataEn = [
-  { id: 1, title: "Mail", text: "silvina.stani@gmail.com" },
-  { id: 2, title: "Country", text: "Argentina" },
-  { id: 3, title: "Phone", text: "+54-91163632288" },
+  { id: 1, title: "Country", text: "Argentina" },
+  { id: 2, title: "Phone", text: "+54-91163632288" },
+  { id: 3, title: "Mail", text: "silvina.stani@gmail.com" },
 ];
