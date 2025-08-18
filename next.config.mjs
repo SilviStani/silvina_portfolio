959b59639cf6ae4ceeb808f2ca3bfdfdf9e6663c
SilviStani <silvina.stani@gmail.com>
2025-08-18 11:55:19 -0300

    change path directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: false, // Cambia a true si necesitas deshabilitar la optimización
+  },
 };
 
 export default nextConfig;
